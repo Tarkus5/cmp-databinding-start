c6dde128dd7f998c74d9520e1f643e9db45a79ea
Alessandro <romano.alessandro.1996@gmail.com>
2023-02-21 11:51:15 +0100

    Lifespan dei componenti

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, Input, OnChanges, OnDestroy, OnInit, SimpleChange, SimpleChanges, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,14 +6,46 @@ import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.Emulated
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
 
   @Input('srvElement')
   element: {type: string, name: string, content: string};
 
-  constructor() { }
+  constructor() {
+    console.log('Constructor called')
+  }
 
-  ngOnInit(): void {
+  ngOnChanges(changes: SimpleChanges) { //Unico hook che richiede un argomento
+    console.log('ngOnChanges called')
+    console.log(changes)
+  }
+
+  ngOnInit() { //Viene chiamato quando la pagina viene inizializzata
+    console.log('ngOnInit called')
+  }
+
+  ngDoCheck() { //Viene triggerato quando viene effettuata una qualsiasi modifica sulla pagina (click di un bottone, scroll, selezione, etc...)
+    console.log('DoCheck called')
+  }
+
+  ngAfterContentInit() { //Viene triggerato una sola volta quando viene inizializzato il contenuto (ma non la sua vista, ancora non è mostrato a schermo)
+    console.log('AfterContentInit called')
+  }
+
+  ngAfterContentChecked() { //Viene chiamato più volte
+    console.log('AfterContentChecked called')
+  }
+
+  ngAfterViewInit() { //Viene chiamato quando viene inizializzata la vista (ma ancora non è stata renderizzata)
+    console.log('AfterViewInit called')
+  }
+
+  ngAfterViewChecked() { //Viene chiamato quando viene renderizzata la vista
+    console.log('AfterViewChecked called')
+  }
+
+  ngOnDestroy() { //Viene chiamato quando finisce il ciclo di vita di un componente
+    console.log('OnDestroy called')
   }
 
 }
